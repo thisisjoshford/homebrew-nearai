bf477f98df6ec377299b3886c6b7a6f5c126a145
Josh Ford <thisisjoshford@gmail.com>
2025-01-21 12:39:35 -0800

    feat: create nearai cli brew tap

---
+++ Formula/nearai.rb
@@ -0,0 +1,20 @@
+class Nearai < Formula
+    include Language::Python::Virtualenv
+    
+    desc "Command-line interface for Near AI"
+    homepage "https://github.com/nearai/nearai"
+    url "https://github.com/nearai/nearai/archive/refs/tags/v0.1.0.tar.gz"
+    sha256 "31d560f542656623a137b3f5b9f79fe6aa737de0268657c94128eea4bc298633"
+    license "MIT"
+  
+    depends_on "python3"
+    
+    def install
+      virtualenv_create(libexec)
+      virtualenv_install_with_resources
+    end
+  
+    test do
+      assert_match "version", shell_output("#{bin}/nearai version")
+    end
+  end
