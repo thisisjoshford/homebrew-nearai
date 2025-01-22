a71665b043ca637ce302a4871ee67be1a04f4d8d
Josh Ford <thisisjoshford@gmail.com>
2025-01-21 16:07:11 -0800

    fix: use ssh instead of https

--- Formula/nearai.rb
+++ Formula/nearai.rb
@@ -2,13 +2,13 @@ class Nearai < Formula
     include Language::Python::Virtualenv
     
     desc "Command-line interface for Near AI"
-    homepage "https://github.com/nearai/nearai"
+    homepage "git@github.com:thisisjoshford/homebrew-nearai.git"
     url "https://github.com/nearai/nearai/archive/refs/tags/v0.1.0.tar.gz"
     sha256 "31d560f542656623a137b3f5b9f79fe6aa737de0268657c94128eea4bc298633"
     license "MIT"
   
     depends_on "python3"
-    
+
     def install
       virtualenv_create(libexec)
       virtualenv_install_with_resources
