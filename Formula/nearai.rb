dd86f5d931f8a9d23168776f19e560c8f795ce31
Josh Ford <thisisjoshford@gmail.com>
2025-01-21 16:14:07 -0800

    fix: revert back to https

--- Formula/nearai.rb
+++ Formula/nearai.rb
@@ -2,7 +2,7 @@ class Nearai < Formula
     include Language::Python::Virtualenv
     
     desc "Command-line interface for Near AI"
-    homepage "git@github.com:thisisjoshford/homebrew-nearai.git"
+    homepage "https://github.com/thisisjoshford/homebrew-nearai"
     url "https://github.com/nearai/nearai/archive/refs/tags/v0.1.0.tar.gz"
     sha256 "31d560f542656623a137b3f5b9f79fe6aa737de0268657c94128eea4bc298633"
     license "MIT"
